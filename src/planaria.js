782b65539f57f86ee9d63f93a3c1c1012cc24837
Brenton Gunning <brentongunning@gmail.com>
2021-02-21 18:34:12 -0700

    Handle reorgs with BitBus

--- src/planaria.js
+++ src/planaria.js
@@ -26,11 +26,13 @@ class Planaria {
     this.logger = logger
     this.abortController = new AbortController()
     this.recrawlInterveral = 10000
+    this.maxReorgDepth = 10
 
     this.txns = []
     this.network = null
     this.recrawlTimerId = null
     this.lastCrawlHeight = null
+    this.pendingReorg = false
   }
 
   async connect (height, network) {
@@ -51,24 +53,36 @@ class Planaria {
   }
 
   async getNextBlock (currHeight, currHash) {
-    while (this.txns.length && this.txns[0].height <= currHeight) {
+    // If we don't have that hash we're looking for next, reorg for safety
+    if (currHash && !this.txns.some(txn => txn.hash === currHash)) {
+      return { reorg: true }
+    }
+
+    // Notify if we've detected a reorg
+    if (this.pendingReorg) {
+      this.pendingReorg = false
+      return { reorg: true }
+    }
+
+    // Remove all transactions that are not realistically reorg-able
+    while (this.txns.length && this.txns[0].height <= currHeight - this.maxReorgDepth) {
       this.txns.shift()
     }
 
-    if (!this.txns.length) return null
+    let i = 0
+    while (i < this.txns.length && this.txns[i].height <= currHeight) { i++ }
+    if (i === this.txns.length) return null
 
     const block = {
-      height: this.txns[0].height,
-      hash: this.txns[0].hash,
+      height: this.txns[i].height,
+      hash: this.txns[i].hash,
       txids: []
     }
 
-    while (this.txns.length && this.txns[0].height === block.height) {
-      block.txids.push(this.txns[0].txid)
-      this.txns.shift()
+    while (i < this.txns.length && this.txns[i].height === block.height) {
+      block.txids.push(this.txns[i].txid)
+      i++
     }
-
-    // TODO: Handle reorgs
 
     return block
   }
@@ -86,7 +100,7 @@ class Planaria {
         find: {
           'out.s2': RUN_PREFIX,
           'out.h3': RUN_VERSION,
-          'blk.i': { $gt: this.lastCrawlHeight }
+          'blk.i': { $gt: this.lastCrawlHeight - this.maxReorgDepth }
         },
         sort: { 'blk.i': 1 },
         project: { blk: 1, 'tx.h': 1 }
@@ -108,13 +122,32 @@ class Planaria {
     return new Promise((resolve, reject) => {
       fetch('https://txo.bitbus.network/block', options)
         .then(res => {
+          let i = 0
+
           res.body.on('end', () => resolve())
             .pipe(es.split())
             .pipe(es.mapSync(json => {
               if (json.length) {
                 const data = JSON.parse(json)
+
+                if (i < this.txns.length) {
+                  if (data.blk.i < this.txns[i].height) {
+                    return
+                  }
+
+                  if (data.blk.i === this.txns[i].height && data.blk.h === this.txns[i].hash) {
+                    return
+                  }
+
+                  if (data.blk.i === this.txns[i].height && data.blk.h !== this.txns[i].hash) {
+                    this.pendingReorg = true
+                    this.txns = this.txns.slice(0, i)
+                  }
+                }
+
                 this.txns.push({ height: data.blk.i, hash: data.blk.h, txid: data.tx.h })
                 this.lastCrawlHeight = data.blk.i
+                i++
               }
             }))
         })
